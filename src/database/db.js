061bfd400c7cd782223faed60c60ba7ec33ce035
prem jethwa <60098511+prem-jethwa@users.noreply.github.com>
2021-05-25 01:01:35 +0530

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const {DB_PASS, DB_USER, DB_NAME = 'task_api', DB_HOST = 'localhost'} = process.env;
+const {DB_PASS, DB_USER, DB_NAME, DB_HOST} = process.env;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   dialect: 'mysql',
@@ -9,14 +9,3 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
 
 module.exports = sequelize;
 
-// TRASH
-// require('mysql2').createPool({
-//   connectionLimit: 1000,
-//   connectTimeout: 60 * 60 * 1000,
-//   acquireTimeout: 60 * 60 * 1000,
-//   timeout: 60 * 60 * 1000,
-//   host: HOST,
-//   user: DB_USER,
-//   password: DB_PASS,
-//   database: DB_NAME,
-// });
