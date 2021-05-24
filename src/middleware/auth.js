bb5cc1d9f67c2138fdede7df2bb1755bbbc5f5b4
prem jethwa <60098511+prem-jethwa@users.noreply.github.com>
2021-05-25 01:04:23 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,19 +2,16 @@ const User = require('../model/user');
 
 const redirectToLogin = async (req, res, next) => {
   if (req.session.isUserAuth) {
-    // const user = await User.findOne({where: {id: req.session.userId}});
 
-    // if (user)
     return next();
-    // res.redirect('/login');
+ 
   }
   res.redirect('/login');
 };
 
 const redirectToHome = async (req, res, next) => {
   if (!req.session.isUserAuth) {
-    // const user = await User.findOne({where: {id: req.session.userId}});
-    // if (!user)
+
     return next();
   }
 
