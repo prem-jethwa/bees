955c3ed15952ccb36485c82d8b58cdceb1f6dd47
prem-jethwa <premjethwa788@gmail.com>
2021-06-17 21:53:34 +0530

    footer update

--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,48 @@
-const main = document.querySelector('main');
+const main = document.querySelector("main");
 let isDescFull = {};
 let allDesc = {};
 
-const moreEl = [...document.querySelectorAll('.more')];
+const moreEl = [...document.querySelectorAll(".more")];
 
-const getTaskId = el => {
-  return el.closest('.task-container').querySelector('.id').textContent.replace('#', '');
+const getTaskId = (el) => {
+  return el
+    .closest(".task-container")
+    .querySelector(".id")
+    .textContent.replace("#", "");
 };
 
-const lessDesc = el => {
+const lessDesc = (el) => {
   const taskId = getTaskId(el);
-  const descEl = el.closest('.desc');
-  const desc = descEl.querySelector('p').textContent.trim();
+  const descEl = el.closest(".desc");
+  const desc = descEl.querySelector("p").textContent.trim();
 
-  const descLength = desc.replaceAll(' ', '').length;
+  const descLength = desc.replaceAll(" ", "").length;
   if (descLength < 50) return;
 
-  descEl.querySelector('p').textContent = `${desc.slice(0, 20 - descLength)}...`;
+  descEl.querySelector("p").textContent = `${desc.slice(
+    0,
+    20 - descLength
+  )}...`;
   allDesc[+taskId] = desc.trim();
   isDescFull[+taskId] = false;
-  el.textContent = 'more';
+  el.textContent = "more";
 };
 
 if (moreEl) {
-  moreEl.map(el => lessDesc(el));
+  moreEl.map((el) => lessDesc(el));
 }
 
-main.addEventListener('click', async e => {
-  const moreEl = e.target.closest('.more');
+main.addEventListener("click", async (e) => {
+  const moreEl = e.target.closest(".more");
   if (moreEl) {
-    const descEl = e.target.closest('.desc');
+    const descEl = e.target.closest(".desc");
     const taskId = getTaskId(moreEl);
 
     if (!isDescFull[taskId]) {
       if (!allDesc[taskId]) return;
 
-      descEl.querySelector('p').textContent = allDesc[taskId];
-      moreEl.textContent = 'less';
+      descEl.querySelector("p").textContent = allDesc[taskId];
+      moreEl.textContent = "less";
 
       return (isDescFull[+taskId] = true);
     }
@@ -45,15 +51,15 @@ main.addEventListener('click', async e => {
   }
 
   //
-  const remove = e.target.closest('.remove');
+  const remove = e.target.closest(".remove");
 
   if (!remove) return;
 
   const url = e.target.dataset.url;
 
   await fetch(url, {
-    method: 'DELETE',
+    method: "DELETE",
   });
 
-  window.location.href = 'https://taskbypj.herokuapp.com/';
+  window.location.href = "https://beesbypj.herokuapp.com/";
 });
