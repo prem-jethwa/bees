a3a815e31bd5b711fa4f13f140bb534052261e2f
prem-jethwa <premjethwa788@gmail.com>
2021-06-18 12:18:25 +0530

    db cleanup update

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-require('mysql2');
-const express = require('express');
-const hbs = require('hbs');
-const path = require('path');
+require("mysql2");
+const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
 const app = express();
 
 // SESSION
-const session = require('express-session');
-const MySqlStore = require('express-mysql-session')(session);
-const cookieParser = require('cookie-parser');
+const session = require("express-session");
+const MySqlStore = require("express-mysql-session")(session);
+const cookieParser = require("cookie-parser");
 
 // env var
-const {DB_PASS, SESS_KEY, DB_NAME, DB_USER, DB_HOST, DB_PORT} = process.env;
+const { DB_PASS, SESS_KEY, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
 
 // Database
-const sequelize = require('./database/db');
-const User = require('./model/user');
-const Task = require('./model/task');
+const sequelize = require("./database/db");
+const User = require("./model/user");
+const Task = require("./model/task");
 
 // Routers
-const taskRouter = require('./routers/task');
-const userRouter = require('./routers/user');
+const taskRouter = require("./routers/task");
+const userRouter = require("./routers/user");
 
 //Formetiong form Data of incoming req
 app.use(express.json());
@@ -31,17 +31,17 @@ app.use(
 
 // Expire database after 1h
 setInterval(async () => {
-  sequelize.sync({force: true});
-}, 1000 * 60 * 60);
+  sequelize.sync({ force: true });
+}, 1000 * 60 * 5);
 
-const publicDirPath = path.join(__dirname, '../public');
-const viewDirPath = path.join(__dirname, '../template');
-const partialsDirPath = path.join(__dirname, '../partials');
+const publicDirPath = path.join(__dirname, "../public");
+const viewDirPath = path.join(__dirname, "../template");
+const partialsDirPath = path.join(__dirname, "../partials");
 
 hbs.registerPartials(partialsDirPath);
 
-app.set('view engine', 'hbs');
-app.set('views', viewDirPath);
+app.set("view engine", "hbs");
+app.set("views", viewDirPath);
 
 // SESSION config
 app.use(cookieParser());
@@ -64,7 +64,7 @@ app.use(sess);
 
 app.use(express.static(publicDirPath));
 
-app.use('/edit/:id', express.static(publicDirPath));
+app.use("/edit/:id", express.static(publicDirPath));
 
 // ROUTERS
 app.use(taskRouter);
@@ -73,14 +73,14 @@ app.use(userRouter);
 // DB relations
 Task.belongsTo(User, {
   constraints: true,
-  onDelete: 'CASCADE',
+  onDelete: "CASCADE",
 });
 User.hasMany(Task);
 
 sequelize
-  // .sync({force: true})
-  .sync()
-  .then(result => console.log('Connected to DATABASE'))
-  .catch(err => console.log(err));
+  .sync({ force: true })
+  // .sync()
+  .then((result) => console.log("Connected to DATABASE"))
+  .catch((err) => console.log(err));
 
 app.listen(process.env.PORT || 3000);
